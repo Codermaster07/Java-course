a2626d78bcb73bb18dcc8883732e18ccfb0f71ab
Harsh Chouksey <2222harshchouksey@gmail.com>
2025-04-17 13:49:07 -0400

    ArraySum

---
+++ SumOf2Arrays.java
@@ -0,0 +1,20 @@
+class SumOf2Arrays
+{
+    public static void main(String[] args) 
+    {
+        int[] arr1={1,2,3,4,5,6};
+        int[] arr2={1,2,3,4,5,6};
+        int[] sum=new int[arr1.length];
+
+        for (int i=0;i<arr1.length;i++)   
+        {
+            sum[i]=arr1[i]+arr2[i];
+        }                                                
+        for (int i=0;i<sum.length;i++)   
+        {
+           System.out.println(sum[i]);
+        }                  
+    
+    }
+    
+}
